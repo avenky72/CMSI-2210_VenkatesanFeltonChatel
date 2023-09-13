df208f7a14b14f91621ce3edc123d4bf484442d4
ChrisChatel <108921036+ChrisChatel@users.noreply.github.com>
2023-09-13 15:22:36 -0700

    Update and rename Classwork03 to Classwork03.c
    updated

---
+++ Classwork/Classwork03.c
@@ -0,0 +1,16 @@
+int main( int argc, char * argv[] ) {
+
+    char outputString[25] = "\0";
+    int index = 0;
+    int value = 0;
+    int del[] = {"Alpha", "Beta", "Charlie", "Delta", "Echo" "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "X-ray", "Yankee", "Zulu"}
+    char * symbol[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+    if ( argc == 1 )
+    printf("Enter the ");
+    scanf("%s", name); 
+    printf("Your name is %s\n");
+
+    return 0;
+}
+
+
