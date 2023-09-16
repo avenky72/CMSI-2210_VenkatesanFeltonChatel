651d36308994713c19d7803131d9186873f7e4cf
Atul Venkatesan <109006663+avenky72@users.noreply.github.com>
2023-09-16 12:36:09 -0700

    Update Classwork03.c

--- Classwork/Classwork03.c
+++ Classwork/Classwork03.c
@@ -1,16 +1,38 @@
-int main( int argc, char * argv[] ) {
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-    char outputString[25] = "\0";
-    int index = 0;
-    int value = 0;
-    int del[] = {"Alpha", "Beta", "Charlie", "Delta", "Echo" "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "X-ray", "Yankee", "Zulu"}
-    char * symbol[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
-    if ( argc == 1 )
-    printf("Enter the ");
-    scanf("%s", name); 
-    printf("Your name is %s\n");
+char* toNATO(char c) {
+    static char* nato[] = {
+        "Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf",
+        "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November",
+        "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform",
+        "Victor", "Whiskey", "X-ray", "Yankee", "Zulu"
+    };
+
+    if (isalpha(c)) {
+        return nato[toupper(c) - 'A'];
+    } else {
+        return "";
+    }
+}
+
+int main() {
+    char input[50];
+    
+    printf("Enter a word or phrase: ");
+    fgets(input, sizeof(input), stdin);
+
+    for (int i = 0; i < strlen(input); i++) {
+        if (input[i] == ' ') {
+            printf("\n");
+        } else {
+            char* nato_word = toNATO(input[i]);
+            if (strlen(nato_word) > 0) {
+                printf("%s ", nato_word);
+            }
+        }
+    }
 
     return 0;
 }
-
-// https://www.tutorialspoint.com/c_standard_library/string_h.htm#:~:text=The%20string.,for%20manipulating%20arrays%20of%20characters.
