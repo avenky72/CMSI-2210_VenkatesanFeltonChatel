3acadb4577b64d2b0ef446a3e41009d56210e0b0
Atul Venkatesan <109006663+avenky72@users.noreply.github.com>
2023-09-16 13:07:46 -0700

    Update Classwork03.c

--- Classwork/Classwork03.c
+++ Classwork/Classwork03.c
@@ -1,38 +1,32 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
 
-char* toNATO(char c) {
-    static char* nato[] = {
+
+int main(int args, char * argv[] ) {
+   
+    char input[50];
+    printf("Enter a word or phrase: ");
+    fgets(input, sizeof(input), stdin);
+
+    char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char* nato[] = {
         "Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf",
         "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November",
         "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform",
         "Victor", "Whiskey", "X-ray", "Yankee", "Zulu"
     };
 
-    if (isalpha(c)) {
-        return nato[toupper(c) - 'A'];
-    } else {
-        return "";
-    }
-}
-
-int main() {
-    char input[50];
-    
-    printf("Enter a word or phrase: ");
-    fgets(input, sizeof(input), stdin);
-
     for (int i = 0; i < strlen(input); i++) {
-        if (input[i] == ' ') {
-            printf("\n");
-        } else {
-            char* nato_word = toNATO(input[i]);
-            if (strlen(nato_word) > 0) {
-                printf("%s ", nato_word);
-            }
+        char c = toupper(input[i]);
+        if (isalpha(c)) {
+            int index = c - 'A'; 
+            printf("%s ", nato[index]); 
+        } else if (c == ' ') { 
+            printf(" "); 
         }
     }
+
+    printf("\n");
 
     return 0;
 }
