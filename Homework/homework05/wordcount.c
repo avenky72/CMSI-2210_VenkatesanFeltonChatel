52bee834e7860b5c539ebd4e05b6e87572a1c228
Atul Venkatesan <109006663+avenky72@users.noreply.github.com>
2023-11-01 11:27:55 -0700

    Create wordcount.c

---
+++ Homework/homework05/wordcount.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define WHITESPACE " \t\n"
+
+int countWords(FILE *file) {
+  int wordCount = 0;
+  char ch;
+  ch = fgetc(file);
+  while (ch != EOF) {
+    if (strchr(WHITESPACE, ch)) {
+      wordCount++;
+    }
+    ch = fgetc(file);
+  }
+  return wordCount;
+}
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("the file is: %s <file_name>\n", argv[0]);
+    exit(1);
+  }
+  FILE *file = fopen(argv[1], "r");
+  if (file == NULL) {
+    printf("can't open file: %s\n", argv[1]);
+    exit(1);
+  }
+  int wordCount = countWords(file);
+  fclose(file);
+  printf("the file %s has %d words.\n", argv[1], wordCount);
+  return 0;
+}
