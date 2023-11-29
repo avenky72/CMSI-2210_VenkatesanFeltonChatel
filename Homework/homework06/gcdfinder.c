9c28be6597f67a1ea77fee913093d4266c723ecc
Atul Venkatesan <109006663+avenky72@users.noreply.github.com>
2023-11-28 22:01:00 -0800

    Create gcdfinder.c

---
+++ Homework/homework06/gcdfinder.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <assert.h>
+
+extern int gcd(int a, int b);
+
+int main() {
+    int num1, num2, result;
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    result = gcd(num1, num2);
+    printf("GCD(%d, %d) = %d\n", num1, num2, result);
+
+    return 0;
+}
